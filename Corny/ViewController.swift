5d417d06aa43026ef7d9245932c8cc25694883be
Ofek Aziel <ofek200@gmail.com>
2020-01-15 10:59:43 +0200

    Initial Commit

---
+++ Corny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Corny
+//
+//  Created by ofek aziel on 15/01/2020.
+//  Copyright © 2020 ofek aziel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
