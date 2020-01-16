a27c83cf356f5ae5cca079ed3eb003655a598f7e
Ofek Aziel <ofek200@gmail.com>
2020-01-16 15:49:43 +0200

    created three new controllers for each screen for login and sign up + project files

---
+++ Corny/ViewControllers/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  Corny
+//
+//  Created by ofek aziel on 16/01/2020.
+//  Copyright © 2020 ofek aziel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+         setUpElements()
+    }
+    
+    func setUpElements() {
+           errorLabel.alpha = 0
+           Utilities.styleTextField(emailTextField)
+           Utilities.styleTextField(passwordTextField)
+           Utilities.styleFilledButton(loginButton)
+       }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+}
---
+++ Corny/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SignUpViewController.swift
+//  Corny
+//
+//  Created by ofek aziel on 16/01/2020.
+//  Copyright © 2020 ofek aziel. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        errorLabel.alpha = 0
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(signUpButton)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+}
