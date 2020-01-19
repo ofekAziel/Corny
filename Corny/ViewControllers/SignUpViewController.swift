0618672e0ab543bea45e1c9f647bb7ab14b7bcdd
Ofek Aziel <ofek200@gmail.com>
2020-01-19 15:06:54 +0200

    added logic to the sign up controller

--- Corny/ViewControllers/SignUpViewController.swift
+++ Corny/ViewControllers/SignUpViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
@@ -24,8 +27,6 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         setUpElements()
     }
     
@@ -38,16 +39,70 @@ class SignUpViewController: UIViewController {
         Utilities.styleFilledButton(signUpButton)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     @IBAction func signUpTapped(_ sender: Any) {
+        let error = validateFields()
+        
+        if error != nil {
+            showError(error!)
+        }
+        else {
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                if err != nil {
+                    self.showError("Error creating user.")
+                }
+                else {
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(data: ["first_name":firstName, "last_name":lastName, "uid":result!.user.uid]) { (error) in
+
+                        if error != nil {
+                            self.showError("Error saving user data.")
+                        }
+                    }
+                }
+            }
+            
+            self.transitionToHome()
+        }
+    }
+    
+    func transitionToHome() {
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
+    
+    func validateFields() -> String? {
+        if isTextFieldsEmpty() {
+            
+            return "Please fill in all fields."
+        }
+        
+        let cleanedPassword =
+            passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            
+            return "Please make sure your password is at least 8 characters, contains a speacial character and a number."
+        }
+        
+        return nil
+    }
+    
+    func isTextFieldsEmpty() -> Bool {
+        return firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+    }
+    
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
     }
 }
