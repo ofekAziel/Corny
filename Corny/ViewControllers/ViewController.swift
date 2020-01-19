0d8498a77b67ef3c1bb436fe3c4b21f77bb88a98
Ofek Aziel <ofek200@gmail.com>
2020-01-19 15:07:31 +0200

    added logic to the view controller, for the video playing in the background

--- Corny/ViewControllers/ViewController.swift
+++ Corny/ViewControllers/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    var videoPlayer: AVPlayer?
+    
+    var videoPlayerLayer: AVPlayerLayer?
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        Utilities.styleFilledButton(signUpButton)
+        Utilities.styleHollowButton(loginButton)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setUpVideo()
+    }
+    
+    func setUpVideo() {
+        let bundlePath = Bundle.main.path(forResource: "loginbg", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        let url = URL(fileURLWithPath: bundlePath!)
+        let item = AVPlayerItem(url: url)
+        videoPlayer = AVPlayer(playerItem: item)
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        videoPlayerLayer?.frame = CGRect(x:
+            -self.view.frame.size.width * 1.5, y: 0, width:
+            self.view.frame.size.width * 4, height:
+            self.view.frame.size.height)
+          
+          view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+          videoPlayer?.playImmediately(atRate: 0.3)
     }
 }
 
