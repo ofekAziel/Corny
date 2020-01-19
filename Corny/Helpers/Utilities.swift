3a24743d27671a6739a6b7db4cc5931c43fed6a4
Ofek Aziel <ofek200@gmail.com>
2020-01-19 16:56:32 +0200

    edited some of the styles of the app

--- Corny/Helpers/Utilities.swift
+++ Corny/Helpers/Utilities.swift
@@ -18,7 +18,7 @@ class Utilities {
         
         bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
         
-        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
+        bottomLine.backgroundColor = UIColor.black.cgColor
         
         // Remove border on text field
         textfield.borderStyle = .none
@@ -31,7 +31,7 @@ class Utilities {
     static func styleFilledButton(_ button:UIButton) {
         
         // Filled rounded corner style
-        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.backgroundColor = UIColor.init(red: 255/255, green: 127/255, blue: 80/255, alpha: 1)
         button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.white
     }
@@ -40,9 +40,9 @@ class Utilities {
         
         // Hollow rounded corner style
         button.layer.borderWidth = 2
-        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderColor = UIColor.white.cgColor
         button.layer.cornerRadius = 25.0
-        button.tintColor = UIColor.black
+        button.tintColor = UIColor.white
     }
     
     static func isPasswordValid(_ password : String) -> Bool {
