b51979bf53943ca1739ed2965d94d8139cdb00c6
Ofek Aziel <ofek200@gmail.com>
2020-01-20 15:58:30 +0200

    changed the name of the home view controller + creating new view controller both in storyboard and swift

---
+++ Corny/ViewControllers/EditMovieViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditMovieViewController.swift
+//  Corny
+//
+//  Created by ofek aziel on 20/01/2020.
+//  Copyright © 2020 ofek aziel. All rights reserved.
+//
+
+import UIKit
+
+class EditMovieViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
