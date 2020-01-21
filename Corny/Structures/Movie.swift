4b44cff621e735ed85a9b4489fa1f4d2471819f5
Ofek Aziel <ofek200@gmail.com>
2020-01-21 15:09:19 +0200

    created movie structure file

---
+++ Corny/Structures/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  Corny
+//
+//  Created by ofek aziel on 21/01/2020.
+//  Copyright © 2020 ofek aziel. All rights reserved.
+//
+
+import Foundation
+
+struct Movie
+{
+    var name:String
+    var genre:String
+    var actors:String
+    var director:String
+    var description:String
+}
